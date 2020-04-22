4cee73d0e4ecf37bf4fe017c8e93f12f7a3b2513
TMax66 <massimo.tranquillo@gmail.com>
2020-04-22 16:33:14 +0200

    update

--- codici.R
+++ codici.R
@@ -3,11 +3,46 @@ library(tidyverse)
 
 dt <- read_excel("subsidenza.xlsx")  
 
-dt %>% 
-  pivot_longer(3:4, names_to = "Operatore", values_to = "grade")
+#dt<-dt %>% 
+#  mutate(diffvet=op1-op2)
+
+dt<-dt %>% 
+  pivot_longer(3:4, names_to = "Operatore", values_to = "grade") %>% 
+  mutate(Operatore=factor(Operatore), 
+         tecnica=factor(tecnica),
+         id=factor(id))
+
+summary(lme4::lmer(grade~tecnica+Operatore+(1|id), data=dt))
+
+
 
 
 #####guarigione#####  
 
-d<-tibble(treatment=c(rep("PD",3), rep("DS",3)),outcome=rep(c("M","S","P"),2),
-          freq=c(5,3,4,11,1,1))
+d<-tibble(treatment=c(rep("PD", 12),rep("DS",13)), 
+          outcome=c(c(rep("M",5 ),rep("S",3), rep("P",4)),
+                    c(rep("M",11),rep("S",1), rep("P",1))))
+                    
+library(nnet)         
+
+m<-multinom(outcome~treatment, data=d)          
+
+library(brms)
+library(bayesplot)
+fit <- brm(
+  formula= outcome~treatment, data=d,
+  family= categorical (link="logit"))
+
+x<-posterior_samples(fit)
+mcmc_intervals(x)
+mcmc_areas(
+ x, 
+ pars = c("b_muP_Intercept", "b_muS_Intercept", "b_muP_treatmentPD",
+          "b_muS_treatmentPD"),
+  prob = 0.8, # 80% intervals
+  prob_outer = 0.99, # 99%
+  point_est = "mean"
+)
+
+
+mcmc_areas_ridges(x)
